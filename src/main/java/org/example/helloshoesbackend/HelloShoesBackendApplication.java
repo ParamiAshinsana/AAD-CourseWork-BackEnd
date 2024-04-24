809a0b99f3c990e1d634d161d16d77abcdef43c9
Parami <paramiashinsana@gmail.com>
2024-04-24 17:30:22 +0530

    first commit

---
+++ src/main/java/org/example/helloshoesbackend/HelloShoesBackendApplication.java
@@ -0,0 +1,13 @@
+package org.example.helloshoesbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloShoesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloShoesBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/helloshoesbackend/HelloShoesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.helloshoesbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloShoesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
