41f2d29400aa09fb08ab8cff879d57ab0bf82ae4
Parami <paramiashinsana@gmail.com>
2024-04-30 20:02:35 +0530

    update CustomerDTO.java

--- src/main/java/org/example/helloshoesbackend/dto/CustomerDTO.java
+++ src/main/java/org/example/helloshoesbackend/dto/CustomerDTO.java
@@ -8,4 +8,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class CustomerDTO implements SuperDTO{
+    private String customerCode;
+    private String customerName;
+    private String customerDOB;
+    private String customerGender;
+    private String customerJoinDate;
+    private String customerLoyaltyPoints;
+    private String loyaltyLevel;
+    private String customerAddress01;
+    private String customerAddress02;
+    private String customerAddress03;
+    private String customerAddress04;
+    private String customerAddress05;
+    private String customerContact;
+    private String customerEmail;
+    private String customerRecDate;
+    private String customerRecTime;
 }
