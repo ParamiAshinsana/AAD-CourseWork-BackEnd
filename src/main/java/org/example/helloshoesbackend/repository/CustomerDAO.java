c64b374d4dcaa3586bb2dbfa0458e07f8d1cadbc
Parami <paramiashinsana@gmail.com>
2024-04-30 19:34:03 +0530

    add CustomerDAO.java

---
+++ src/main/java/org/example/helloshoesbackend/repository/CustomerDAO.java
@@ -0,0 +1,9 @@
+package org.example.helloshoesbackend.repository;
+
+import org.example.helloshoesbackend.entity.CustomerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerDAO extends JpaRepository<CustomerEntity, String> {
+}
