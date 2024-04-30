36c8b6c93bc4c1b54ec5d7da2fab051cdf58ce44
Parami <paramiashinsana@gmail.com>
2024-04-30 19:41:11 +0530

    update CustomerController.java

--- src/main/java/org/example/helloshoesbackend/controller/CustomerController.java
+++ src/main/java/org/example/helloshoesbackend/controller/CustomerController.java
@@ -1,4 +1,11 @@
 package org.example.helloshoesbackend.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/customers")
+@RequiredArgsConstructor
 public class CustomerController {
 }
