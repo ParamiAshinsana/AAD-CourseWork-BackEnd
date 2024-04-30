723c9ace3fd7efe7dab4d11480169a58f3400473
Parami <paramiashinsana@gmail.com>
2024-04-30 22:35:22 +0530

    update CustomerServiceIMPL.java

--- src/main/java/org/example/helloshoesbackend/service/impl/CustomerServiceIMPL.java
+++ src/main/java/org/example/helloshoesbackend/service/impl/CustomerServiceIMPL.java
@@ -4,12 +4,14 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.example.helloshoesbackend.dto.CustomerDTO;
 import org.example.helloshoesbackend.entity.CustomerEntity;
+import org.example.helloshoesbackend.exception.NotFoundException;
 import org.example.helloshoesbackend.repository.CustomerDAO;
 import org.example.helloshoesbackend.service.CustomerService;
 import org.example.helloshoesbackend.utill.CustomerMapping;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -43,6 +45,18 @@ public class CustomerServiceIMPL implements CustomerService {
 
     @Override
     public void updateCustomer(String id, CustomerDTO customerDTO) {
-
+        Optional<CustomerEntity> tmpCustomer = customerDAO.findById(id);
+        if(!tmpCustomer.isPresent()) throw new NotFoundException("CustomerController not found");
+        tmpCustomer.get().setCustomerName(customerDTO.getCustomerName());
+        tmpCustomer.get().setCustomerDOB(customerDTO.getCustomerDOB());
+        tmpCustomer.get().setCustomerGender(customerDTO.getCustomerGender());
+        tmpCustomer.get().setCustomerJoinDate(customerDTO.getCustomerJoinDate());
+        tmpCustomer.get().setCustomerLoyaltyPoints(customerDTO.getCustomerLoyaltyPoints());
+        tmpCustomer.get().setLoyaltyLevel(customerDTO.getLoyaltyLevel());
+        tmpCustomer.get().setCustomerAddress(customerDTO.getCustomerAddress());
+        tmpCustomer.get().setCustomerContact(customerDTO.getCustomerContact());
+        tmpCustomer.get().setCustomerEmail(customerDTO.getCustomerEmail());
+        tmpCustomer.get().setCustomerRecDate(customerDTO.getCustomerRecDate());
+        tmpCustomer.get().setCustomerRecTime(customerDTO.getCustomerRecTime());
     }
 }
