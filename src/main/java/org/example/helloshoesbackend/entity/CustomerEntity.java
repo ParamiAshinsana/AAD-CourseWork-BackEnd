1545e881844bfdbde58aca9556794e6f1d663ce4
Parami <paramiashinsana@gmail.com>
2024-04-30 20:03:39 +0530

    update CustomerEntity.java

--- src/main/java/org/example/helloshoesbackend/entity/CustomerEntity.java
+++ src/main/java/org/example/helloshoesbackend/entity/CustomerEntity.java
@@ -1,6 +1,7 @@
 package org.example.helloshoesbackend.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -12,4 +13,21 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "customer")
 public class CustomerEntity {
+    @Id
+    private String customerCode;
+    private String customerName;
+    private String customerDOB;
+    private String customerGender;
+    private String customerJoinDate;
+    private String customerLoyaltyPoints;
+    private String loyaltyLevel;
+    private String customerAddress01;
+    private String customerAddress02;
+    private String customerAddress03;
+    private String customerAddress04;
+    private String customerAddress05;
+    private String customerContact;
+    private String customerEmail;
+    private String customerRecDate;
+    private String customerRecTime;
 }
