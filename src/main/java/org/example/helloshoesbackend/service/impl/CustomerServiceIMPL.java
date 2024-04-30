0d515ed27c81d90e609181ce317f58e3aa6cc6f9
Parami <paramiashinsana@gmail.com>
2024-04-30 22:07:51 +0530

    update CustomerServiceIMPL.java

--- src/main/java/org/example/helloshoesbackend/service/impl/CustomerServiceIMPL.java
+++ src/main/java/org/example/helloshoesbackend/service/impl/CustomerServiceIMPL.java
@@ -3,6 +3,7 @@ package org.example.helloshoesbackend.service.impl;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.example.helloshoesbackend.dto.CustomerDTO;
+import org.example.helloshoesbackend.entity.CustomerEntity;
 import org.example.helloshoesbackend.repository.CustomerDAO;
 import org.example.helloshoesbackend.service.CustomerService;
 import org.example.helloshoesbackend.utill.CustomerMapping;
@@ -19,7 +20,10 @@ public class CustomerServiceIMPL implements CustomerService {
 
     @Override
     public CustomerDTO saveCustomer(CustomerDTO customerDTO) {
-        return null;
+        CustomerEntity customerEntity = customermapping.toCustomer(customerDTO);
+
+        customerEntity = customerDAO.save(customerEntity);
+        return customermapping.toCustomerDTO(customerEntity);
     }
 
     @Override
