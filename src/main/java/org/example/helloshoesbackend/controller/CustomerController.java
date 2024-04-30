69bb89316b0dd71fe981c889b66901a99aa26fb7
Parami <paramiashinsana@gmail.com>
2024-04-30 22:06:49 +0530

    update CustomerController.java

--- src/main/java/org/example/helloshoesbackend/controller/CustomerController.java
+++ src/main/java/org/example/helloshoesbackend/controller/CustomerController.java
@@ -1,6 +1,10 @@
 package org.example.helloshoesbackend.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.helloshoesbackend.dto.CustomerDTO;
+import org.example.helloshoesbackend.service.CustomerService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +12,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/customers")
 @RequiredArgsConstructor
 public class CustomerController {
+    private final CustomerService customerService;
+
+    @PostMapping
+    public CustomerDTO saveCustomer(@RequestBody CustomerDTO customerDTO){
+        return customerService.saveCustomer(customerDTO);
+    }
 }
