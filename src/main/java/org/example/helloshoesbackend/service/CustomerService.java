da8128f2a7220e297dc707f6d0e1440be8e007ac
Parami <paramiashinsana@gmail.com>
2024-04-30 19:39:03 +0530

    add CustomerService.java

---
+++ src/main/java/org/example/helloshoesbackend/service/CustomerService.java
@@ -0,0 +1,13 @@
+package org.example.helloshoesbackend.service;
+
+import org.example.helloshoesbackend.dto.CustomerDTO;
+
+import java.util.List;
+
+public interface CustomerService {
+    CustomerDTO saveCustomer(CustomerDTO customerDTO);
+    List<CustomerDTO> getAllCustomer();
+    CustomerDTO getSelectedCustomer(String id);
+    void deleteCustomer(String id);
+    void updateCustomer(String id, CustomerDTO customerDTO);
+}
