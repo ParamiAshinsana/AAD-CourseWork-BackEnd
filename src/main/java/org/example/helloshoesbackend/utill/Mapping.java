6464d31517faa6a6f79cde5595a74aa8e09a7530
Parami <paramiashinsana@gmail.com>
2024-04-30 20:06:57 +0530

    update Mapping.java

---
+++ src/main/java/org/example/helloshoesbackend/utill/Mapping.java
@@ -0,0 +1,25 @@
+package org.example.helloshoesbackend.utill;
+
+import lombok.RequiredArgsConstructor;
+import org.example.helloshoesbackend.dto.CustomerDTO;
+import org.example.helloshoesbackend.entity.CustomerEntity;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class Mapping {
+    private final ModelMapper mapper;
+
+    public CustomerDTO toCustomerDTO(CustomerEntity customer) {
+        return  mapper.map(customer, CustomerDTO.class);
+    }
+    public CustomerEntity toCustomer(CustomerDTO customerDTO) {
+        return  mapper.map(customerDTO, CustomerEntity.class);
+    }
+    public List<CustomerDTO> toCustomerDTOList(List<CustomerEntity> customers) {
+        return mapper.map(customers, List.class);
+    }
+}
