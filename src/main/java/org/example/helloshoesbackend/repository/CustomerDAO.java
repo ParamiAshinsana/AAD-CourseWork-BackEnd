dc270ec9e70878ecf907ae63cd5e14cc7aa4e46f
Parami <paramiashinsana@gmail.com>
2024-05-02 20:40:20 +0530

    update

--- src/main/java/org/example/helloshoesbackend/repository/CustomerDAO.java
+++ src/main/java/org/example/helloshoesbackend/repository/CustomerDAO.java
@@ -2,6 +2,7 @@ package org.example.helloshoesbackend.repository;
 
 import org.example.helloshoesbackend.entity.CustomerEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
