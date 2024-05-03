52d11936ee1e5210970cacde71097a138ed8cb3a
Parami <paramiashinsana@gmail.com>
2024-05-03 12:11:48 +0530

    update SupplierServiceIMPL

--- src/main/java/org/example/helloshoesbackend/service/impl/SupplierServiceIMPL.java
+++ src/main/java/org/example/helloshoesbackend/service/impl/SupplierServiceIMPL.java
@@ -2,10 +2,38 @@ package org.example.helloshoesbackend.service.impl;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.example.helloshoesbackend.dto.SupplierDTO;
+import org.example.helloshoesbackend.service.SupplierService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class SupplierServiceIMPL {
+public class SupplierServiceIMPL implements SupplierService {
+    @Override
+    public SupplierDTO saveSupplier(SupplierDTO supplierDTO) {
+        return null;
+    }
+
+    @Override
+    public List<SupplierDTO> getAllSupplier() {
+        return null;
+    }
+
+    @Override
+    public SupplierDTO getSelectedSupplier(String id) {
+        return null;
+    }
+
+    @Override
+    public void deleteSupplier(String id) {
+
+    }
+
+    @Override
+    public void updateSupplier(String id, SupplierDTO supplierDTO) {
+
+    }
 }
