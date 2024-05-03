83f0d32304bfc366cce6402aafafb04427eaf3c8
Parami <paramiashinsana@gmail.com>
2024-05-03 17:53:29 +0530

    update SupplierServiceIMPL

--- src/main/java/org/example/helloshoesbackend/service/impl/SupplierServiceIMPL.java
+++ src/main/java/org/example/helloshoesbackend/service/impl/SupplierServiceIMPL.java
@@ -3,7 +3,10 @@ package org.example.helloshoesbackend.service.impl;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.example.helloshoesbackend.dto.SupplierDTO;
+import org.example.helloshoesbackend.entity.SupplierEntity;
+import org.example.helloshoesbackend.repository.SupplierDAO;
 import org.example.helloshoesbackend.service.SupplierService;
+import org.example.helloshoesbackend.utill.SupplierMapping;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,9 +15,15 @@ import java.util.List;
 @Transactional
 @RequiredArgsConstructor
 public class SupplierServiceIMPL implements SupplierService {
+    private final SupplierDAO supplierDAO;
+    private final SupplierMapping supplierMapping;
+
     @Override
     public SupplierDTO saveSupplier(SupplierDTO supplierDTO) {
-        return null;
+        SupplierEntity supplierEntity = supplierMapping.toSupplier(supplierDTO);
+
+        supplierEntity = supplierDAO.save(supplierEntity);
+        return supplierMapping.toSupplierDTO(supplierEntity);
     }
 
     @Override
