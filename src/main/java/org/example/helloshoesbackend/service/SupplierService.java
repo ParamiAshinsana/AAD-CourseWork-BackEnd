5fa564b0e4bf22c8a0d9e82bfe03c00e00b88f27
Parami <paramiashinsana@gmail.com>
2024-05-03 12:10:23 +0530

    update SupplierService

--- src/main/java/org/example/helloshoesbackend/service/SupplierService.java
+++ src/main/java/org/example/helloshoesbackend/service/SupplierService.java
@@ -1,4 +1,13 @@
 package org.example.helloshoesbackend.service;
 
+import org.example.helloshoesbackend.dto.SupplierDTO;
+
+import java.util.List;
+
 public interface SupplierService {
+    SupplierDTO saveSupplier(SupplierDTO supplierDTO);
+    List<SupplierDTO> getAllSupplier();
+    SupplierDTO getSelectedSupplier(String id);
+    void deleteSupplier(String id);
+    void updateSupplier(String id, SupplierDTO supplierDTO);
 }
