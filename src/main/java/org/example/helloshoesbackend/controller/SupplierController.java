083a8d60c65c0c6deea3a4c57e929ea9f178369a
Parami <paramiashinsana@gmail.com>
2024-05-03 12:09:09 +0530

    update SupplierController

--- src/main/java/org/example/helloshoesbackend/controller/SupplierController.java
+++ src/main/java/org/example/helloshoesbackend/controller/SupplierController.java
@@ -1,6 +1,7 @@
 package org.example.helloshoesbackend.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.helloshoesbackend.service.SupplierService;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,4 +11,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/suppliers")
 @RequiredArgsConstructor
 public class SupplierController {
+    private final SupplierService supplierService;
 }
