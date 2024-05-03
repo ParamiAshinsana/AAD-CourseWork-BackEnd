fa40f96a627d16d866fbeb86d85a6c2545695a85
Parami <paramiashinsana@gmail.com>
2024-05-03 17:53:52 +0530

    update SupplierMapping

--- src/main/java/org/example/helloshoesbackend/utill/SupplierMapping.java
+++ src/main/java/org/example/helloshoesbackend/utill/SupplierMapping.java
@@ -1,4 +1,27 @@
 package org.example.helloshoesbackend.utill;
 
+import lombok.RequiredArgsConstructor;
+import org.example.helloshoesbackend.dto.SuperDTO;
+import org.example.helloshoesbackend.dto.SupplierDTO;
+import org.example.helloshoesbackend.entity.CustomerEntity;
+import org.example.helloshoesbackend.entity.SupplierEntity;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
 public class SupplierMapping {
+    private final ModelMapper mapper;
+
+    public SupplierDTO toSupplierDTO(SupplierEntity supplierEntity) {
+        return  mapper.map(supplierEntity, SupplierDTO.class);
+    }
+    public SupplierEntity toSupplier(SupplierDTO supplierDTO) {
+        return  mapper.map(supplierDTO, SupplierEntity.class);
+    }
+    public List<SupplierDTO> toSupplierDTOList(List<SupplierEntity> supplierEntities) {
+        return mapper.map(supplierEntities, List.class);
+    }
 }
