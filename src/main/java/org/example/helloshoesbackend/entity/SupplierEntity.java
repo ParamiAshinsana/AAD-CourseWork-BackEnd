29d9194715dbf741385735f8e573de43efbef9c0
Parami <paramiashinsana@gmail.com>
2024-05-03 12:42:05 +0530

    update SupplierEntity

--- src/main/java/org/example/helloshoesbackend/entity/SupplierEntity.java
+++ src/main/java/org/example/helloshoesbackend/entity/SupplierEntity.java
@@ -1,7 +1,6 @@
 package org.example.helloshoesbackend.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +9,20 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 @Entity
-@Table(name = "customer")
+@Table(name = "supplier")
 public class SupplierEntity {
+    @Id
+    private String supplierCode;
+    private String supplierName;
+    @Enumerated(EnumType.STRING)
+    private Category category;
+    private String customerAddress01;
+    private String customerAddress02;
+    private String customerAddress03;
+    private String customerAddress04;
+    private String customerAddress05;
+    private String customerAddress06;
+    private String ContactNo01;
+    private String ContactNo02;
+    private String supplierEmail;
 }
