59958416c403523a4c330b74e9a51beac96f3c08
Parami <paramiashinsana@gmail.com>
2024-05-03 12:38:54 +0530

    update SupplierDTO

--- src/main/java/org/example/helloshoesbackend/dto/SupplierDTO.java
+++ src/main/java/org/example/helloshoesbackend/dto/SupplierDTO.java
@@ -3,9 +3,22 @@ package org.example.helloshoesbackend.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.example.helloshoesbackend.entity.Category;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class SupplierDTO implements SuperDTO{
+    private String supplierCode;
+    private String supplierName;
+    private Category category;
+    private String customerAddress01;
+    private String customerAddress02;
+    private String customerAddress03;
+    private String customerAddress04;
+    private String customerAddress05;
+    private String customerAddress06;
+    private String ContactNo01;
+    private String ContactNo02;
+    private String supplierEmail;
 }
