43ac364f658664c408163c68831ea9b95444902c
Parami <paramiashinsana@gmail.com>
2024-05-05 22:53:45 +0530

    update InventoryService

--- src/main/java/org/example/helloshoesbackend/service/InventoryService.java
+++ src/main/java/org/example/helloshoesbackend/service/InventoryService.java
@@ -2,6 +2,9 @@ package org.example.helloshoesbackend.service;
 
 import org.example.helloshoesbackend.dto.InventoryDTO;
 
+import java.util.List;
+
 public interface InventoryService {
-    InventoryDTO getSuppliersCodes(InventoryDTO inventoryDTO);
+
+    List<InventoryDTO> getSuppliersCodes();
 }
