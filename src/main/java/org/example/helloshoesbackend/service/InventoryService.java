c5a6a5cbe553086b4750ecd57a3339648d09eac1
Parami <paramiashinsana@gmail.com>
2024-05-05 22:29:55 +0530

    add InventoryService

---
+++ src/main/java/org/example/helloshoesbackend/service/InventoryService.java
@@ -0,0 +1,7 @@
+package org.example.helloshoesbackend.service;
+
+import org.example.helloshoesbackend.dto.InventoryDTO;
+
+public interface InventoryService {
+    InventoryDTO getSuppliersCodes(InventoryDTO inventoryDTO);
+}
