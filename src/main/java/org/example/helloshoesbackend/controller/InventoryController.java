bfd8a7d050c939ad5be305826027e692e042d785
Parami <paramiashinsana@gmail.com>
2024-05-05 22:29:08 +0530

    update InventoryController

--- src/main/java/org/example/helloshoesbackend/controller/InventoryController.java
+++ src/main/java/org/example/helloshoesbackend/controller/InventoryController.java
@@ -10,4 +10,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/inventory")
 @RequiredArgsConstructor
 public class InventoryController {
+    private final InventoryService inventoryService;
 }
