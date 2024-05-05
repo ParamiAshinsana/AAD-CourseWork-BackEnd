3f9b3bdb2d25f2ab3dfcc7c181727adb2bcf752c
Parami <paramiashinsana@gmail.com>
2024-05-05 22:53:01 +0530

    update InventoryController

--- src/main/java/org/example/helloshoesbackend/controller/InventoryController.java
+++ src/main/java/org/example/helloshoesbackend/controller/InventoryController.java
@@ -1,9 +1,15 @@
 package org.example.helloshoesbackend.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.helloshoesbackend.dto.CustomerDTO;
+import org.example.helloshoesbackend.dto.InventoryDTO;
+import org.example.helloshoesbackend.service.InventoryService;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @CrossOrigin
 @RestController
@@ -11,4 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class InventoryController {
     private final InventoryService inventoryService;
+
+    @GetMapping(value = "/getAllSuppliersCodes")
+    List<InventoryDTO> getAllSuppliers(){
+        return inventoryService.getSuppliersCodes();
+    }
 }
