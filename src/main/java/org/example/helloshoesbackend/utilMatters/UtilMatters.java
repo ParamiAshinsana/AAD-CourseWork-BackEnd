d4dbd2343e5f044788626e0c0b568294c8ee355a
Parami <paramiashinsana@gmail.com>
2024-05-07 04:40:34 +0530

    add UtilMatters.java

---
+++ src/main/java/org/example/helloshoesbackend/utilMatters/UtilMatters.java
@@ -0,0 +1,17 @@
+package org.example.helloshoesbackend.utilMatters;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Base64;
+
+public class UtilMatters {
+    public static String convertBase64(MultipartFile data) {
+        try {
+            return Base64.getEncoder().encodeToString(data.getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
