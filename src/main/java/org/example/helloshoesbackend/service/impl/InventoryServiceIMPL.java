d5d6987c1c0136cec2215842cd231f17ebd49841
Parami <paramiashinsana@gmail.com>
2024-05-07 04:37:47 +0530

    update InventoryServiceIMPL

--- src/main/java/org/example/helloshoesbackend/service/impl/InventoryServiceIMPL.java
+++ src/main/java/org/example/helloshoesbackend/service/impl/InventoryServiceIMPL.java
@@ -2,16 +2,17 @@ package org.example.helloshoesbackend.service.impl;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.example.helloshoesbackend.dto.CustomerDTO;
 import org.example.helloshoesbackend.dto.InventoryDTO;
-import org.example.helloshoesbackend.repository.CustomerDAO;
+import org.example.helloshoesbackend.entity.CustomerEntity;
+import org.example.helloshoesbackend.entity.InventoryEntity;
+import org.example.helloshoesbackend.entity.SupplierEntity;
 import org.example.helloshoesbackend.repository.InventoryDAO;
 import org.example.helloshoesbackend.service.InventoryService;
-import org.example.helloshoesbackend.utill.CustomerMapping;
 import org.example.helloshoesbackend.utill.InventoryMapping;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -20,9 +21,6 @@ public class InventoryServiceIMPL implements InventoryService {
     private final InventoryDAO inventoryDAO;
     private final InventoryMapping inventoryMapping;
 
-    @Override
-    public List<InventoryDTO> getSuppliersCodes() {
-        return inventoryMapping.toSupplierDTOList(inventoryDAO.findAll());
-    }
+
 
 }
