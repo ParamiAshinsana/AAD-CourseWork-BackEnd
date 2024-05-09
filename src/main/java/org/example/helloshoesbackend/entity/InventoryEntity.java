2958f1f60798dc391a8ca1948fe34fd87a91fd3f
Parami <paramiashinsana@gmail.com>
2024-05-09 21:08:48 +0530

    update InventoryEntity

--- src/main/java/org/example/helloshoesbackend/entity/InventoryEntity.java
+++ src/main/java/org/example/helloshoesbackend/entity/InventoryEntity.java
@@ -14,6 +14,7 @@ public class InventoryEntity {
     @Id
     private String itemCode;
     private String itemDescription;
+    @Column(columnDefinition = "LONGTEXT")
     private String itemPicture;
     private String category;
     private int itemQty;
