4f68ee78e6637f1f5213f5613ba7dea989ae450c
Parami <paramiashinsana@gmail.com>
2024-05-09 21:16:55 +0530

    update SupplierDAO

--- src/main/java/org/example/helloshoesbackend/repository/SupplierDAO.java
+++ src/main/java/org/example/helloshoesbackend/repository/SupplierDAO.java
@@ -2,8 +2,16 @@ package org.example.helloshoesbackend.repository;
 
 import org.example.helloshoesbackend.entity.SupplierEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface SupplierDAO extends JpaRepository<SupplierEntity, String> {
+    @Query(value = "SELECT supplierName FROM Supplier WHERE supplierCode = ?1", nativeQuery = true)
+    String findNameById(String id);
+
+    @Query(value = "SELECT supplierCode FROM Supplier", nativeQuery = true)
+    List<String> findSupplierCodes();
 }
