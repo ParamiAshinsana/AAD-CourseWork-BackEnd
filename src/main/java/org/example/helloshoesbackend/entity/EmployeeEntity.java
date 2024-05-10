8c347aac86a51c443db30a7d64e348ab60b11fd0
Parami <paramiashinsana@gmail.com>
2024-05-10 14:31:59 +0530

    add the annotations to EmployeeEntity

--- src/main/java/org/example/helloshoesbackend/entity/EmployeeEntity.java
+++ src/main/java/org/example/helloshoesbackend/entity/EmployeeEntity.java
@@ -1,4 +1,15 @@
 package org.example.helloshoesbackend.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "employee")
 public class EmployeeEntity {
 }
