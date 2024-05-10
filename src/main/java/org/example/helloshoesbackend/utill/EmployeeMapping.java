9e4f62cadd48a27a3e723278465576c1485c8dff
Parami <paramiashinsana@gmail.com>
2024-05-10 14:33:00 +0530

    add the annotations to EmployeeMapping

--- src/main/java/org/example/helloshoesbackend/utill/EmployeeMapping.java
+++ src/main/java/org/example/helloshoesbackend/utill/EmployeeMapping.java
@@ -1,4 +1,9 @@
 package org.example.helloshoesbackend.utill;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class EmployeeMapping {
 }
