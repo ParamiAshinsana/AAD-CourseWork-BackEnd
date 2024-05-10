6047630c6a60a2f0530509847308dfdba78d0516
Parami <paramiashinsana@gmail.com>
2024-05-10 15:02:34 +0530

    update EmployeeDTO

--- src/main/java/org/example/helloshoesbackend/dto/EmployeeDTO.java
+++ src/main/java/org/example/helloshoesbackend/dto/EmployeeDTO.java
@@ -3,9 +3,28 @@ package org.example.helloshoesbackend.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.example.helloshoesbackend.entity.Gender;
+
+import java.util.Date;
 
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
 public class EmployeeDTO {
+    private String employeeCode;
+    private String employeeName;
+    private String employeeProfilePicture;
+    private Gender employeeGender;
+    private String employeeStatus;
+    private String employeeDesignation;
+    private String employeeAccessRole;
+    private Date employeeDob;
+    private Date employeeJoinedDate;
+    private String attachedBranch;
+    private String employeeAddress;
+    private String employeeContact;
+    private String employeeEmail;
+    private String nameTheGuardian;
+    private String emergencyContact;
+
 }
