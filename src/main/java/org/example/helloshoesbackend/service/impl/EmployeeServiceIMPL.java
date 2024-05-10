6f79ba041fb717db6bcf6aee441240b3ee414d7b
Parami <paramiashinsana@gmail.com>
2024-05-10 14:36:55 +0530

    update EmployeeServiceIMPL

--- src/main/java/org/example/helloshoesbackend/service/impl/EmployeeServiceIMPL.java
+++ src/main/java/org/example/helloshoesbackend/service/impl/EmployeeServiceIMPL.java
@@ -2,10 +2,38 @@ package org.example.helloshoesbackend.service.impl;
 
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
+import org.example.helloshoesbackend.dto.EmployeeDTO;
+import org.example.helloshoesbackend.service.EmployeeService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class EmployeeServiceIMPL {
+public class EmployeeServiceIMPL implements EmployeeService {
+    @Override
+    public EmployeeDTO saveEmployee(EmployeeDTO employeeDTO) {
+        return null;
+    }
+
+    @Override
+    public List<EmployeeDTO> getAllEmployee() {
+        return null;
+    }
+
+    @Override
+    public EmployeeDTO getSelectedEmployee(String id) {
+        return null;
+    }
+
+    @Override
+    public void deleteEmployee(String id) {
+
+    }
+
+    @Override
+    public void updateEmployee(String id, EmployeeDTO employeeDTO) {
+
+    }
 }
