d94c98fc9f2275ad03925cf25589ce92d1ef61dd
Parami <paramiashinsana@gmail.com>
2024-05-10 15:04:22 +0530

    update EmployeeEntity

--- src/main/java/org/example/helloshoesbackend/entity/EmployeeEntity.java
+++ src/main/java/org/example/helloshoesbackend/entity/EmployeeEntity.java
@@ -1,10 +1,11 @@
 package org.example.helloshoesbackend.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.Date;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -12,4 +13,22 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "employee")
 public class EmployeeEntity {
+    @Id
+    private String employeeCode;
+    private String employeeName;
+    private String employeeProfilePicture;
+    @Enumerated(EnumType.STRING)
+    private Gender employeeGender;
+    private String employeeStatus;
+    private String employeeDesignation;
+    @Enumerated(EnumType.STRING)
+    private AccessRole employeeAccessRole;
+    private Date employeeDob;
+    private Date employeeJoinedDate;
+    private String attachedBranch;
+    private String employeeAddress;
+    private String employeeContact;
+    private String employeeEmail;
+    private String nameTheGuardian;
+    private String emergencyContact;
 }
