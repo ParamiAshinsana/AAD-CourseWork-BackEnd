b4e2812310c98c1d3297c0329ee79f89e0882952
Parami <paramiashinsana@gmail.com>
2024-05-10 20:12:56 +0530

    update EmployeeService

--- src/main/java/org/example/helloshoesbackend/service/EmployeeService.java
+++ src/main/java/org/example/helloshoesbackend/service/EmployeeService.java
@@ -1,12 +1,12 @@
 package org.example.helloshoesbackend.service;
 
-import org.example.helloshoesbackend.dto.CustomerDTO;
 import org.example.helloshoesbackend.dto.EmployeeDTO;
 
 import java.util.List;
 
 public interface EmployeeService {
     EmployeeDTO saveEmployee(EmployeeDTO employeeDTO);
+
     List<EmployeeDTO> getAllEmployee();
     EmployeeDTO getSelectedEmployee(String id);
     void deleteEmployee(String id);
