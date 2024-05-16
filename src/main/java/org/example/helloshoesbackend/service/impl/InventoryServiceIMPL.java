e979c854ee85387bb7a080185a9000067d4eabc4
Parami <paramiashinsana@gmail.com>
2024-05-16 20:33:55 +0530

    update InventoryServiceIMPL

--- src/main/java/org/example/helloshoesbackend/service/impl/InventoryServiceIMPL.java
+++ src/main/java/org/example/helloshoesbackend/service/impl/InventoryServiceIMPL.java
@@ -4,6 +4,7 @@ import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.example.helloshoesbackend.dto.InventoryDTO;
 import org.example.helloshoesbackend.entity.InventoryEntity;
+import org.example.helloshoesbackend.exception.NotFoundException;
 import org.example.helloshoesbackend.repository.InventoryDAO;
 import org.example.helloshoesbackend.service.InventoryService;
 import org.example.helloshoesbackend.utill.InventoryMapping;
@@ -36,6 +37,7 @@ public class InventoryServiceIMPL implements InventoryService {
 
     @Override
     public void deleteInventory(String id) {
-
+        if(!inventoryDAO.existsById(id)) throw new NotFoundException("Inventory not found");
+        inventoryDAO.deleteById(id);
     }
 }
