f703c20116b9c089a1d940ac6b881f65b1c5798d
Parami <paramiashinsana@gmail.com>
2024-05-16 11:30:17 +0530

    update EmployeeServiceIMPL

--- src/main/java/org/example/helloshoesbackend/service/impl/EmployeeServiceIMPL.java
+++ src/main/java/org/example/helloshoesbackend/service/impl/EmployeeServiceIMPL.java
@@ -3,14 +3,18 @@ package org.example.helloshoesbackend.service.impl;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.example.helloshoesbackend.dto.EmployeeDTO;
+import org.example.helloshoesbackend.entity.AccessRole;
+import org.example.helloshoesbackend.entity.CustomerEntity;
 import org.example.helloshoesbackend.entity.EmployeeEntity;
 import org.example.helloshoesbackend.exception.NotFoundException;
 import org.example.helloshoesbackend.repository.EmployeeDAO;
 import org.example.helloshoesbackend.service.EmployeeService;
+import org.example.helloshoesbackend.utilMatters.UtilMatters;
 import org.example.helloshoesbackend.utill.EmployeeMapping;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -44,7 +48,28 @@ public class EmployeeServiceIMPL implements EmployeeService {
 
     @Override
     public EmployeeDTO updateEmployee(String id, EmployeeDTO employeeDTO) {
+        Optional<EmployeeEntity> tmpEmployee = employeeDAO.findById(id);
+        if (!tmpEmployee.isPresent()) {
+            throw new NotFoundException("Employee not found with code: " + id);
+        }
 
-        return employeeDTO;
+        // Update the employee details
+        tmpEmployee.get().setEmployeeName(employeeDTO.getEmployeeName());
+        tmpEmployee.get().setEmployeeProfilePicture(employeeDTO.getEmployeeProfilePicture());
+        tmpEmployee.get().setEmployeeDob(employeeDTO.getEmployeeDob());
+        tmpEmployee.get().setEmployeeGender(employeeDTO.getEmployeeGender());
+        tmpEmployee.get().setEmployeeStatus(employeeDTO.getEmployeeStatus());
+        tmpEmployee.get().setEmployeeDesignation(employeeDTO.getEmployeeDesignation());
+        tmpEmployee.get().setEmployeeAccessRole(AccessRole.valueOf(employeeDTO.getEmployeeAccessRole()));
+        tmpEmployee.get().setEmployeeJoinedDate(employeeDTO.getEmployeeJoinedDate());
+        tmpEmployee.get().setAttachedBranch(employeeDTO.getAttachedBranch());
+        tmpEmployee.get().setEmployeeAddress(employeeDTO.getEmployeeAddress());
+        tmpEmployee.get().setEmployeeContact(employeeDTO.getEmployeeContact());
+        tmpEmployee.get().setEmployeeEmail(employeeDTO.getEmployeeEmail());
+        tmpEmployee.get().setNameTheGuardian(employeeDTO.getNameTheGuardian());
+        tmpEmployee.get().setEmergencyContact(employeeDTO.getEmergencyContact());
+
+        return employeeMapping.toEmployeeDTO(tmpEmployee.get());
+
     }
 }
