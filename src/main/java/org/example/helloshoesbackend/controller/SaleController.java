df5965da4ab1f22ac43bbe806fef8f33d944cbb6
Parami <paramiashinsana@gmail.com>
2024-05-17 18:27:29 +0530

    update SaleController

--- src/main/java/org/example/helloshoesbackend/controller/SaleController.java
+++ src/main/java/org/example/helloshoesbackend/controller/SaleController.java
@@ -1,6 +1,7 @@
 package org.example.helloshoesbackend.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.helloshoesbackend.service.SaleService;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,4 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/sales")
 @RequiredArgsConstructor
 public class SaleController {
+
+    private final SaleService saleService;
 }
