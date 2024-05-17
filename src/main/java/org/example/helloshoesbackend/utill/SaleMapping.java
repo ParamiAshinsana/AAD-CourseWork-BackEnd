c5ccf0ac0e0e0d05f3646533dcadba7cff74f6c3
Parami <paramiashinsana@gmail.com>
2024-05-17 13:38:40 +0530

    add SaleMapping

---
+++ src/main/java/org/example/helloshoesbackend/utill/SaleMapping.java
@@ -0,0 +1,10 @@
+package org.example.helloshoesbackend.utill;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SaleMapping {
+}
