753794146dc739837bd60e399b1388d5f0abb3bd
Parami <paramiashinsana@gmail.com>
2024-05-17 21:05:20 +0530

    update InventoryDAO

--- src/main/java/org/example/helloshoesbackend/repository/InventoryDAO.java
+++ src/main/java/org/example/helloshoesbackend/repository/InventoryDAO.java
@@ -2,11 +2,13 @@ package org.example.helloshoesbackend.repository;
 
 import org.example.helloshoesbackend.entity.InventoryEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface InventoryDAO extends JpaRepository<InventoryEntity, String> {
+    @Query(value = "SELECT itemCode FROM Inventory", nativeQuery = true)
     List<String> findItemCodes();
 }
