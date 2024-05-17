13dc22c371a977a9b5ec9f94671caa4c3047e2b4
Parami <paramiashinsana@gmail.com>
2024-05-17 13:36:04 +0530

    add SaleEntity

---
+++ src/main/java/org/example/helloshoesbackend/entity/SaleEntity.java
@@ -0,0 +1,15 @@
+package org.example.helloshoesbackend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+@Table(name = "sale")
+public class SaleEntity {
+}
