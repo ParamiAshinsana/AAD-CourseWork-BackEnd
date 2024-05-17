7900dc4c5b3c08c4a27ffdf3311ae62e4778f3d3
Parami <paramiashinsana@gmail.com>
2024-05-17 18:28:33 +0530

    update SaleDAO

--- src/main/java/org/example/helloshoesbackend/repository/SaleDAO.java
+++ src/main/java/org/example/helloshoesbackend/repository/SaleDAO.java
@@ -1,7 +1,9 @@
 package org.example.helloshoesbackend.repository;
 
+import org.example.helloshoesbackend.entity.SaleEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SaleDAO {
+public interface SaleDAO extends JpaRepository<SaleEntity, String> {
 }
