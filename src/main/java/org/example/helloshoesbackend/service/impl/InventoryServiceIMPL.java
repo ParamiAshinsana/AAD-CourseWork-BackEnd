846e5b0486eb8d864582059bdde103bfaaa07972
Parami <paramiashinsana@gmail.com>
2024-05-17 21:29:28 +0530

    update InventoryServiceIMPL

--- src/main/java/org/example/helloshoesbackend/service/impl/InventoryServiceIMPL.java
+++ src/main/java/org/example/helloshoesbackend/service/impl/InventoryServiceIMPL.java
@@ -47,4 +47,9 @@ public class InventoryServiceIMPL implements InventoryService {
     public List<String> getAllItemCodes() {
         return inventoryDAO.findItemCodes();
     }
+
+    @Override
+    public String getInventoryDetails(String id) {
+        return null;
+    }
 }
