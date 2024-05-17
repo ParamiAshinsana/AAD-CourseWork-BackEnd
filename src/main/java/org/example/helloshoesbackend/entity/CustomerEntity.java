930557b1ba409106b6d2125ff21cec273e3cff6a
Parami <paramiashinsana@gmail.com>
2024-05-17 18:13:00 +0530

    update CustomerEntity

--- src/main/java/org/example/helloshoesbackend/entity/CustomerEntity.java
+++ src/main/java/org/example/helloshoesbackend/entity/CustomerEntity.java
@@ -33,6 +33,6 @@ public class CustomerEntity {
     private String customerContact;
     private String customerEmail;
 
-    @OneToMany
+    @OneToMany(mappedBy = "customerEntity")
     private List<SaleEntity> saleEntities ;
 }
