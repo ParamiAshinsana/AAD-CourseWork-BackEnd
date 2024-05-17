e51f237941eceb53cd15c1ae4fbe26be71fe3955
Parami <paramiashinsana@gmail.com>
2024-05-17 13:37:52 +0530

    add SaleServiceIMPL

---
+++ src/main/java/org/example/helloshoesbackend/service/impl/SaleServiceIMPL.java
@@ -0,0 +1,11 @@
+package org.example.helloshoesbackend.service.impl;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class SaleServiceIMPL {
+}
