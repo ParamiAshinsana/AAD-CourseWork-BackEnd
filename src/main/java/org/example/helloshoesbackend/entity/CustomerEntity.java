e5a30a92cb1989a1c785f69320df499beadb40f7
Parami <paramiashinsana@gmail.com>
2024-05-17 18:11:30 +0530

    update CustomerEntity

--- src/main/java/org/example/helloshoesbackend/entity/CustomerEntity.java
+++ src/main/java/org/example/helloshoesbackend/entity/CustomerEntity.java
@@ -5,6 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @AllArgsConstructor
 @NoArgsConstructor
 @Data
@@ -30,4 +32,7 @@ public class CustomerEntity {
 //    private String customerAddress05;
     private String customerContact;
     private String customerEmail;
+
+    @OneToMany
+    private List<SaleEntity> saleEntities ;
 }
