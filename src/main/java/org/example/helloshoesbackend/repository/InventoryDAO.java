57e7bf23b6056b10cfb97db7b858d7650a85587c
Parami <paramiashinsana@gmail.com>
2024-05-18 14:47:31 +0530

    update

--- src/main/java/org/example/helloshoesbackend/repository/InventoryDAO.java
+++ src/main/java/org/example/helloshoesbackend/repository/InventoryDAO.java
@@ -12,14 +12,14 @@ public interface InventoryDAO extends JpaRepository<InventoryEntity, String> {
     @Query(value = "SELECT itemCode FROM Inventory", nativeQuery = true)
     List<String> findItemCodes();
 
-//    @Query(value = "SELECT  itemDescription, unitPriceSale, itemSize FROM Inventory WHERE itemCode = ?1", nativeQuery = true)
-    @Query(value = "SELECT itemDescription FROM Inventory WHERE itemCode = ?1", nativeQuery = true)
+    @Query(value = "SELECT  itemDescription, unitPriceSale, itemSize FROM Inventory WHERE itemCode = ?1", nativeQuery = true)
+//    @Query(value = "SELECT itemDescription FROM Inventory WHERE itemCode = ?1", nativeQuery = true)
     String findInventoryDescriptionById(String id);
 
 
-    @Query(value = "SELECT unitPriceSale FROM Inventory WHERE itemCode = ?1", nativeQuery = true)
-    String findInventoryPricessById(String id);
-
-    @Query(value = "SELECT itemSize FROM Inventory WHERE itemCode = ?1", nativeQuery = true)
-    String findShoeSizeById(String id);
+//    @Query(value = "SELECT unitPriceSale FROM Inventory WHERE itemCode = ?1", nativeQuery = true)
+//    String findInventoryPricessById(String id);
+//
+//    @Query(value = "SELECT itemSize FROM Inventory WHERE itemCode = ?1", nativeQuery = true)
+//    String findShoeSizeById(String id);
 }
