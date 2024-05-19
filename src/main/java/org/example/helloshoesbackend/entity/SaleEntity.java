81f24cb3553b215fb59585896afe6f025ec59f28
Parami <paramiashinsana@gmail.com>
2024-05-19 17:56:51 +0530

    update SaleEntity

--- src/main/java/org/example/helloshoesbackend/entity/SaleEntity.java
+++ src/main/java/org/example/helloshoesbackend/entity/SaleEntity.java
@@ -4,13 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter
+@Setter
 @Entity
 @Table(name = "sale")
 public class SaleEntity {
