5af3726949a5c4e17142625596a10e11c7ea8d23
Parami <paramiashinsana@gmail.com>
2024-05-19 17:55:16 +0530

    update InventoryDAO

--- src/main/java/org/example/helloshoesbackend/repository/InventoryDAO.java
+++ src/main/java/org/example/helloshoesbackend/repository/InventoryDAO.java
@@ -12,9 +12,9 @@ public interface InventoryDAO extends JpaRepository<InventoryEntity, String> {
     @Query(value = "SELECT itemCode FROM Inventory", nativeQuery = true)
     List<String> findItemCodes();
 
-    @Query(value = "SELECT  itemDescription, unitPriceSale, itemSize FROM Inventory WHERE itemCode = ?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM Inventory WHERE itemCode = ?1", nativeQuery = true)
 //    @Query(value = "SELECT itemDescription FROM Inventory WHERE itemCode = ?1", nativeQuery = true)
-    String findInventoryDescriptionById(String id);
+    List<InventoryEntity> findInventoryDescriptionById(String id);
 
 
     @Query(value = "SELECT unitPriceSale FROM Inventory WHERE itemCode = ?1", nativeQuery = true)
