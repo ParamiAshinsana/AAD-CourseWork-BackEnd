c648bd665a03671eee4459f3d608006eddf0687f
Parami <paramiashinsana@gmail.com>
2024-05-19 17:54:30 +0530

    update EmployeeEntity

--- src/main/java/org/example/helloshoesbackend/entity/EmployeeEntity.java
+++ src/main/java/org/example/helloshoesbackend/entity/EmployeeEntity.java
@@ -1,15 +1,14 @@
 package org.example.helloshoesbackend.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDate;
 
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Getter
+@Setter
 @Entity
 @Table(name = "employee")
 public class EmployeeEntity {
