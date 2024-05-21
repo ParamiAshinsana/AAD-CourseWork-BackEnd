7b75b54e52a5fcda93c19d96ce894f224281c16a
Parami <paramiashinsana@gmail.com>
2024-05-21 18:20:51 +0530

    update SaleServiceIMPL

--- src/main/java/org/example/helloshoesbackend/service/impl/SaleServiceIMPL.java
+++ src/main/java/org/example/helloshoesbackend/service/impl/SaleServiceIMPL.java
@@ -3,14 +3,18 @@ package org.example.helloshoesbackend.service.impl;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.example.helloshoesbackend.dto.SaleDTO;
-import org.example.helloshoesbackend.service.InventoryService;
+import org.example.helloshoesbackend.repository.SaleDAO;
 import org.example.helloshoesbackend.service.SaleService;
+import org.example.helloshoesbackend.utill.SaleMapping;
 import org.springframework.stereotype.Service;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class SaleServiceIMPL implements SaleService {
+
+    private final SaleDAO saleDAO;
+    private final SaleMapping saleMapping;
 
     @Override
     public SaleDTO saveSale(SaleDTO saleDTO) {
