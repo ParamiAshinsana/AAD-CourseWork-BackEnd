3352223b6f96bcb7d8eb0c2e629e025d94f5d489
Parami <paramiashinsana@gmail.com>
2024-05-21 18:20:38 +0530

    update SaleMapping

--- src/main/java/org/example/helloshoesbackend/utill/SaleMapping.java
+++ src/main/java/org/example/helloshoesbackend/utill/SaleMapping.java
@@ -2,9 +2,26 @@ package org.example.helloshoesbackend.utill;
 
 
 import lombok.RequiredArgsConstructor;
+
+import org.example.helloshoesbackend.dto.SaleDTO;
+import org.example.helloshoesbackend.entity.SaleEntity;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class SaleMapping {
+    private final ModelMapper mapper;
+
+    public SaleDTO toSaleDTO(SaleEntity saleEntity) {
+        return  mapper.map(saleEntity, SaleDTO.class);
+    }
+    public SaleEntity toSale(SaleDTO saleDTO) {
+        return  mapper.map(saleDTO, SaleEntity.class);
+    }
+    public List<SaleDTO> toSaleDTOList(List<SaleEntity> saleEntities) {
+        return mapper.map(saleEntities, List.class);
+    }
 }
