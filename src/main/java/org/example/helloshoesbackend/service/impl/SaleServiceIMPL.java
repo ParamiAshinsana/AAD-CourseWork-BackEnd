e32e5a127a55766e84985032d9b13f95fdb266dc
Parami <paramiashinsana@gmail.com>
2024-05-21 18:58:58 +0530

    update SaleServiceIMPL

--- src/main/java/org/example/helloshoesbackend/service/impl/SaleServiceIMPL.java
+++ src/main/java/org/example/helloshoesbackend/service/impl/SaleServiceIMPL.java
@@ -3,6 +3,8 @@ package org.example.helloshoesbackend.service.impl;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.example.helloshoesbackend.dto.SaleDTO;
+import org.example.helloshoesbackend.entity.CustomerEntity;
+import org.example.helloshoesbackend.entity.SaleEntity;
 import org.example.helloshoesbackend.repository.SaleDAO;
 import org.example.helloshoesbackend.service.SaleService;
 import org.example.helloshoesbackend.utill.SaleMapping;
@@ -18,6 +20,9 @@ public class SaleServiceIMPL implements SaleService {
 
     @Override
     public SaleDTO saveSale(SaleDTO saleDTO) {
-        return null;
+        SaleEntity saleEntity = saleMapping.toSale(saleDTO);
+
+        saleEntity = saleDAO.save(saleEntity);
+        return saleMapping.toSaleDTO(saleEntity);
     }
 }
