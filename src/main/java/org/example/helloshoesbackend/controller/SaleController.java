fcad46c0cc6989b729b62361084db8561303adfd
Parami <paramiashinsana@gmail.com>
2024-05-22 23:27:48 +0530

    update

--- src/main/java/org/example/helloshoesbackend/controller/SaleController.java
+++ src/main/java/org/example/helloshoesbackend/controller/SaleController.java
@@ -2,6 +2,7 @@ package org.example.helloshoesbackend.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.example.helloshoesbackend.dto.CustomerDTO;
+import org.example.helloshoesbackend.dto.InventoryDTO;
 import org.example.helloshoesbackend.dto.SaleDTO;
 import org.example.helloshoesbackend.dto.SupplierDTO;
 import org.example.helloshoesbackend.service.SaleService;
@@ -14,9 +15,20 @@ import org.springframework.web.bind.annotation.*;
 public class SaleController {
 
     private final SaleService saleService;
+    SaleDTO salesDTO = new SaleDTO();
 
     @PostMapping(value = "/saveSale")
     public SaleDTO saveSales(@RequestBody SaleDTO saleDTO){
+
+        CustomerDTO customerDTO = new CustomerDTO();
+        customerDTO.setCustomerCode(String.valueOf(customerDTO));
+        salesDTO.setCusDTO(customerDTO);
+
+        InventoryDTO inventoryDTO = new InventoryDTO();
+        inventoryDTO.setItemCode(String.valueOf(inventoryDTO));
+        salesDTO.setInvDTO(inventoryDTO);
+
         return saleService.saveSale(saleDTO);
     }
 }
+
