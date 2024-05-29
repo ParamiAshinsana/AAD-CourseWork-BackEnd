6e3b0167e2e4cfbe476497252f7dc0320e1f1c69
Parami <paramiashinsana@gmail.com>
2024-05-29 17:00:51 +0530

    remove unnecessary lines in SaleController

--- src/main/java/org/example/helloshoesbackend/controller/SaleController.java
+++ src/main/java/org/example/helloshoesbackend/controller/SaleController.java
@@ -1,10 +1,7 @@
 package org.example.helloshoesbackend.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.example.helloshoesbackend.dto.CustomerDTO;
-import org.example.helloshoesbackend.dto.InventoryDTO;
 import org.example.helloshoesbackend.dto.SaleDTO;
-import org.example.helloshoesbackend.dto.SupplierDTO;
 import org.example.helloshoesbackend.service.SaleService;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,39 +12,12 @@ import org.springframework.web.bind.annotation.*;
 public class SaleController {
 
     private final SaleService saleService;
-    SaleDTO salesDTO = new SaleDTO();
 
     @PostMapping(value = "/saveSale")
     public SaleDTO saveSales(@RequestBody SaleDTO saleDTO) {
         return saleService.saveSale(saleDTO);
     }
 
-//    @PostMapping(value = "/saveSale")
-//    public SaleDTO saveSales(@RequestBody SaleDTO saleDTO){
-//
-//        CustomerDTO customerDTO = new CustomerDTO();
-//        customerDTO.setCustomerCode(String.valueOf(customerDTO));
-//        salesDTO.setCusDTO(customerDTO);
-//
-//        InventoryDTO inventoryDTO = new InventoryDTO();
-//        inventoryDTO.setItemCode(String.valueOf(inventoryDTO));
-//        salesDTO.setInvDTO(inventoryDTO);
-//
-//        return saleService.saveSale(saleDTO);
-//    }
 
-
-//    @PostMapping(value = "/saveSale")
-//    public SaleDTO saveSales(@RequestBody SaleDTO saleDTO, @RequestParam String cusID, @RequestParam String invId) {
-//        CustomerDTO customerDTO = new CustomerDTO();
-//        customerDTO.setCustomerCode(cusID);
-//        saleDTO.setCusDTO(customerDTO);
-//
-//        InventoryDTO inventoryDTO = new InventoryDTO();
-//        inventoryDTO.setItemCode(invId);
-//        saleDTO.setInvDTO(inventoryDTO);
-//
-//        return saleService.saveSale(saleDTO);
-//    }
 }
 
