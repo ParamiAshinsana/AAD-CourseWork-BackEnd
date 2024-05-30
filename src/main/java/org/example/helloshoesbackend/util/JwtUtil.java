d8ca4e91609442bc52ce7807fbfcce6b01f5b859
Parami <paramiashinsana@gmail.com>
2024-05-30 20:42:12 +0530

    add @PropertySource

--- src/main/java/org/example/helloshoesbackend/util/JwtUtil.java
+++ src/main/java/org/example/helloshoesbackend/util/JwtUtil.java
@@ -1,8 +1,10 @@
 package org.example.helloshoesbackend.util;
 
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
+@PropertySource(ignoreResourceNotFound = true, value = "classpath:otherprops.properties")
 public class JwtUtil {
 
 }
