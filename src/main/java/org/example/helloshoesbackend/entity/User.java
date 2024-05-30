2d882ef38aa2a69828c097f3803557b93bc1b9ea
Parami <paramiashinsana@gmail.com>
2024-05-30 21:59:50 +0530

    update User.java

--- src/main/java/org/example/helloshoesbackend/entity/User.java
+++ src/main/java/org/example/helloshoesbackend/entity/User.java
@@ -1,13 +1,13 @@
 package org.example.helloshoesbackend.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.util.UUID;
 
 @Entity
 @Table(name = "systemuser")
@@ -16,4 +16,12 @@ import java.io.Serializable;
 @Getter
 @Setter
 public class User implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID uid;
+    @Column(unique = true)
+    private String email;
+    private String password;
+    private String name;
+    private String role;
 }
