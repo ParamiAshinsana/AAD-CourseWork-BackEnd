1c3ff9f94bb237ac6ab7f15b3e9f417247f64569
Parami <paramiashinsana@gmail.com>
2024-05-30 21:27:11 +0530

    add @EnableWebSecurity

--- src/main/java/org/example/helloshoesbackend/config/WebSecurityConfig.java
+++ src/main/java/org/example/helloshoesbackend/config/WebSecurityConfig.java
@@ -1,4 +1,7 @@
 package org.example.helloshoesbackend.config;
 
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
 public class WebSecurityConfig {
 }
