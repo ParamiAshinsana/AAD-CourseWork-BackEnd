acc71e7f23cdc8838bfcb0673074f1a127042d6f
Parami <paramiashinsana@gmail.com>
2024-05-30 21:24:56 +0530

    update JwtUtil Class

--- src/main/java/org/example/helloshoesbackend/util/JwtUtil.java
+++ src/main/java/org/example/helloshoesbackend/util/JwtUtil.java
@@ -1,10 +1,22 @@
 package org.example.helloshoesbackend.util;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 
 @Component
 @PropertySource(ignoreResourceNotFound = true, value = "classpath:otherprops.properties")
 public class JwtUtil {
+
+    //The serialVersionUID is a unique identifier used in Java serialization
+    // to ensure that a serialized object can be deserialized correctly,
+    // even if the class definition has changed
+    private static final long serialVersionUID = 234234523523L;
+
+    public static final long JWT_TOKEN_VALIDITY = 24 * 60 * 60 * 12;
+
+
+    @Value("${jwt.secret}")
+    private String secretKey;
 
 }
