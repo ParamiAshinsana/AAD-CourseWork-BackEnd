09bd196b9683b0c73e8a852218593c980214d5c3
Parami <paramiashinsana@gmail.com>
2024-05-30 21:48:33 +0530

    add @CrossOrigin in AuthController

--- src/main/java/org/example/helloshoesbackend/controller/AuthController.java
+++ src/main/java/org/example/helloshoesbackend/controller/AuthController.java
@@ -1,4 +1,11 @@
 package org.example.helloshoesbackend.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+@RequestMapping("api/v1/auth")
 public class AuthController {
 }
