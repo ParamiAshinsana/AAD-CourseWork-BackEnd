6e0f7ed670b0b38fa3c092d5176c02b05acfeefa
Parami <paramiashinsana@gmail.com>
2024-05-30 22:02:03 +0530

    update UserService

--- src/main/java/org/example/helloshoesbackend/service/UserService.java
+++ src/main/java/org/example/helloshoesbackend/service/UserService.java
@@ -1,5 +1,7 @@
 package org.example.helloshoesbackend.service;
 
+import org.example.helloshoesbackend.dto.UserDTO;
+
 public interface UserService {
     int saveUser(UserDTO userDTO);
     UserDTO searchUser(String username);
