5668a94a8239a3beace007ffe30d42338859650d
Parami <paramiashinsana@gmail.com>
2024-05-30 21:46:24 +0530

    update WebSecurityConfig

--- src/main/java/org/example/helloshoesbackend/config/WebSecurityConfig.java
+++ src/main/java/org/example/helloshoesbackend/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.example.helloshoesbackend.service.impl.UserServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -21,5 +22,9 @@ public class WebSecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userService).passwordEncoder(passwordEncoder());
     }
 }
