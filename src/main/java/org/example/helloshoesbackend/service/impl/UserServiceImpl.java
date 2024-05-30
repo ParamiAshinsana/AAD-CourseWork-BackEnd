f0e3b9423061754ea037b40353b8d2a8fade7d5c
Parami <paramiashinsana@gmail.com>
2024-05-30 22:02:43 +0530

    update UserServiceImpl

--- src/main/java/org/example/helloshoesbackend/service/impl/UserServiceImpl.java
+++ src/main/java/org/example/helloshoesbackend/service/impl/UserServiceImpl.java
@@ -1,4 +1,9 @@
 package org.example.helloshoesbackend.service.impl;
 
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
 public class UserServiceImpl {
 }
