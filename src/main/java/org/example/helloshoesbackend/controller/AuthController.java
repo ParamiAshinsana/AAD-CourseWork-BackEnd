30f9a5766dddaa4543e7d09120f46be3b511d123
Parami <paramiashinsana@gmail.com>
2024-05-30 21:49:23 +0530

    add properties in AuthController

--- src/main/java/org/example/helloshoesbackend/controller/AuthController.java
+++ src/main/java/org/example/helloshoesbackend/controller/AuthController.java
@@ -1,5 +1,6 @@
 package org.example.helloshoesbackend.controller;
 
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,4 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/auth")
 public class AuthController {
+    private final JwtUtil jwtUtil;
+    private final AuthenticationManager authenticationManager;
+    private final UserServiceImpl userService;
+    private final ResponseDTO responseDTO;
 }
