702c403b6fcc97bcf0aa4f161617dd02ae8e874f
Parami <paramiashinsana@gmail.com>
2024-05-30 22:03:44 +0530

    update UserServiceImpl

--- src/main/java/org/example/helloshoesbackend/service/impl/UserServiceImpl.java
+++ src/main/java/org/example/helloshoesbackend/service/impl/UserServiceImpl.java
@@ -1,11 +1,28 @@
 package org.example.helloshoesbackend.service.impl;
 
+import org.example.helloshoesbackend.dto.UserDTO;
 import org.example.helloshoesbackend.service.UserService;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 public class UserServiceImpl implements UserDetailsService, UserService {
+    @Override
+    public int saveUser(UserDTO userDTO) {
+        return 0;
+    }
+
+    @Override
+    public UserDTO searchUser(String username) {
+        return null;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
 }
