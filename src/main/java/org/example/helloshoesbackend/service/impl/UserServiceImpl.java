491a0fcfd65eda9e636b7e6d3755aa83054423f6
Parami <paramiashinsana@gmail.com>
2024-05-30 22:03:00 +0530

    update UserServiceImpl

--- src/main/java/org/example/helloshoesbackend/service/impl/UserServiceImpl.java
+++ src/main/java/org/example/helloshoesbackend/service/impl/UserServiceImpl.java
@@ -1,9 +1,11 @@
 package org.example.helloshoesbackend.service.impl;
 
+import org.example.helloshoesbackend.service.UserService;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
-public class UserServiceImpl {
+public class UserServiceImpl implements UserDetailsService, UserService {
 }
