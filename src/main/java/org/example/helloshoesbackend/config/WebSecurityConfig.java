75c383bbc4005031c979547e4a50d3459ed9050a
Parami <paramiashinsana@gmail.com>
2024-05-30 21:43:25 +0530

    add Autowired propertiesin WebSecurityConfig

--- src/main/java/org/example/helloshoesbackend/config/WebSecurityConfig.java
+++ src/main/java/org/example/helloshoesbackend/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package org.example.helloshoesbackend.config;
 
+import org.example.helloshoesbackend.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,4 +10,8 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 @Configuration
 @EnableMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
+    @Autowired
+    private UserServiceImpl userService;
+    @Autowired
+    private JwtFilter jwtFilter;
 }
