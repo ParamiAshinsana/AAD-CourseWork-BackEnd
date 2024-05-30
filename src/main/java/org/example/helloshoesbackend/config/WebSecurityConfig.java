cb39b51831d773542af133cae68debc2e846b61a
Parami <paramiashinsana@gmail.com>
2024-05-30 21:28:22 +0530

    add @EnableMethodSecurity(prePostEnabled = true)

--- src/main/java/org/example/helloshoesbackend/config/WebSecurityConfig.java
+++ src/main/java/org/example/helloshoesbackend/config/WebSecurityConfig.java
@@ -1,9 +1,11 @@
 package org.example.helloshoesbackend.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @EnableWebSecurity
 @Configuration
+@EnableMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig {
 }
