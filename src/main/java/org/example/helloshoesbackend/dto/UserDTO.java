c569089cf2fda4635142d5e77d43bc8e78750c10
Parami <paramiashinsana@gmail.com>
2024-05-30 21:58:01 +0530

    update UserDTO

--- src/main/java/org/example/helloshoesbackend/dto/UserDTO.java
+++ src/main/java/org/example/helloshoesbackend/dto/UserDTO.java
@@ -8,4 +8,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class UserDTO {
+    private String email;
+    private String password;
+    private String name;
+    private String role;
 }
