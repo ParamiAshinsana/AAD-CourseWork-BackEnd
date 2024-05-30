523330fb4692bf1d5ee8c0f95b478a2bbf398bbf
Parami <paramiashinsana@gmail.com>
2024-05-30 22:04:54 +0530

    update UserServiceImpl

--- src/main/java/org/example/helloshoesbackend/service/impl/UserServiceImpl.java
+++ src/main/java/org/example/helloshoesbackend/service/impl/UserServiceImpl.java
@@ -5,11 +5,16 @@ import org.example.helloshoesbackend.repository.UserRepository;
 import org.example.helloshoesbackend.service.UserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @Service
 @Transactional
@@ -21,5 +26,43 @@ public class UserServiceImpl implements UserDetailsService, UserService {
     @Autowired
     private ModelMapper modelMapper;
 
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email);
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), getAuthority(user));
+    }
+
+    public UserDTO loadUserDetailsByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        return modelMapper.map(user,UserDTO.class);
+    }
+
+    private Set<SimpleGrantedAuthority> getAuthority(User user) {
+        Set<SimpleGrantedAuthority> authorities = new HashSet<>();
+        authorities.add(new SimpleGrantedAuthority(user.getRole()));
+        return authorities;
+    }
+
+    @Override
+    public UserDTO searchUser(String username) {
+        if (userRepository.existsByEmail(username)) {
+            User user=userRepository.findByEmail(username);
+            return modelMapper.map(user,UserDTO.class);
+        } else {
+            return null;
+        }
+    }
+
+    @Override
+    public int saveUser(UserDTO userDTO) {
+        if (userRepository.existsByEmail(userDTO.getEmail())) {
+            return 0;
+        } else {
+            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+            userDTO.setPassword(passwordEncoder.encode(userDTO.getPassword()));
+            userRepository.save(modelMapper.map(userDTO, User.class));
+            return 1;
+        }
+    }
 
 }
