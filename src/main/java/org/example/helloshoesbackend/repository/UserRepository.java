01bc9db3632bc8602f0132681b874f6bc74fef7d
Parami <paramiashinsana@gmail.com>
2024-05-30 22:01:14 +0530

    update UserRepository

--- src/main/java/org/example/helloshoesbackend/repository/UserRepository.java
+++ src/main/java/org/example/helloshoesbackend/repository/UserRepository.java
@@ -5,4 +5,10 @@ import org.example.helloshoesbackend.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User,String> {
+
+    User findByEmail(String userName);
+
+    boolean existsByEmail(String userName);
+
+    int deleteByEmail(String userName);
 }
