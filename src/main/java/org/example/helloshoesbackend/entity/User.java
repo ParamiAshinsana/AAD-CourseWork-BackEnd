36e7ef69d3c2fcb3cce500b122a6bc508fd16706
Parami <paramiashinsana@gmail.com>
2024-05-30 21:59:12 +0530

    update User.java

--- src/main/java/org/example/helloshoesbackend/entity/User.java
+++ src/main/java/org/example/helloshoesbackend/entity/User.java
@@ -1,4 +1,17 @@
 package org.example.helloshoesbackend.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "systemuser")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class User {
 }
