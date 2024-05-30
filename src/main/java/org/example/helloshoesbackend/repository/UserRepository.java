40014f8e9cdd8ea6b916a9ff1639aa851acb02a3
Parami <paramiashinsana@gmail.com>
2024-05-30 22:00:56 +0530

    update UserRepository

--- src/main/java/org/example/helloshoesbackend/repository/UserRepository.java
+++ src/main/java/org/example/helloshoesbackend/repository/UserRepository.java
@@ -1,5 +1,8 @@
 package org.example.helloshoesbackend.repository;
 
 
+import org.example.helloshoesbackend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 public interface UserRepository extends JpaRepository<User,String> {
 }
