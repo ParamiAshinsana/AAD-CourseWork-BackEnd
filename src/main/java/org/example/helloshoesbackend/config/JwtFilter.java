dcd155af24bc8288cf1c64180ec2f999503acaf6
Parami <paramiashinsana@gmail.com>
2024-05-30 19:52:39 +0530

    update JwtFilter Class

--- src/main/java/org/example/helloshoesbackend/config/JwtFilter.java
+++ src/main/java/org/example/helloshoesbackend/config/JwtFilter.java
@@ -1,7 +1,8 @@
 package org.example.helloshoesbackend.config;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
-public class JwtFilter {
+public class JwtFilter extends OncePerRequestFilter {
 }
