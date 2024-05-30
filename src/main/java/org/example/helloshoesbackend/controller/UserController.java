98bfab7f2400ccb07b8ce34526a9f1772292b366
Parami <paramiashinsana@gmail.com>
2024-05-30 21:53:59 +0530

    update UserController

--- src/main/java/org/example/helloshoesbackend/controller/UserController.java
+++ src/main/java/org/example/helloshoesbackend/controller/UserController.java
@@ -2,9 +2,9 @@ package org.example.helloshoesbackend.controller;
 
 import org.example.helloshoesbackend.service.UserService;
 import org.example.helloshoesbackend.util.JwtUtil;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
@@ -17,5 +17,33 @@ public class UserController {
     public UserController(UserService userService, JwtUtil jwtUtil) {
         this.userService = userService;
         this.jwtUtil = jwtUtil;
+    }
+
+    @PostMapping(value = "/register")
+    public ResponseEntity<ResponseDTO> registerUser(@RequestBody UserDTO userDTO) {
+        try {
+            int res = userService.saveUser(userDTO);
+            switch (res) {
+                case 1 -> {
+                    String token = jwtUtil.generateToken(userDTO);
+                    AuthDTO authDTO = new AuthDTO();
+                    authDTO.setEmail(userDTO.getEmail());
+                    authDTO.setToken(token);
+                    return ResponseEntity.status(HttpStatus.CREATED)
+                            .body(new ResponseDTO(200, "Success", authDTO));
+                }
+                case 0 -> {
+                    return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE)
+                            .body(new ResponseDTO(402, "Email Already Used", null));
+                }
+                default -> {
+                    return ResponseEntity.status(HttpStatus.BAD_GATEWAY)
+                            .body(new ResponseDTO(405, "Error", null));
+                }
+            }
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(new ResponseDTO(500, e.getMessage(), null));
+        }
     }
 }
