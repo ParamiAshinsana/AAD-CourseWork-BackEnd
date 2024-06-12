f17236da323616a91f9080084b4a8e99790929b5
Parami <paramiashinsana@gmail.com>
2024-06-12 13:00:56 +0530

    Update

--- src/main/java/org/example/helloshoesbackend/repository/SaleDAO.java
+++ src/main/java/org/example/helloshoesbackend/repository/SaleDAO.java
@@ -2,8 +2,18 @@ package org.example.helloshoesbackend.repository;
 
 import org.example.helloshoesbackend.entity.SaleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Repository
 public interface SaleDAO extends JpaRepository<SaleEntity, String> {
+    @Query("SELECT s.inventoryEntities.itemCode, s.inventoryEntities.itemDescription, SUM(s.orderItemQty) AS totalQty " +
+            "FROM SaleEntity s WHERE s.purchaseDate = :date " +
+            "GROUP BY s.inventoryEntities.itemCode, s.inventoryEntities.itemDescription " +
+            "ORDER BY totalQty DESC")
+    List<Object[]> findBestSellingInventory(@Param("date") LocalDate date);
 }
