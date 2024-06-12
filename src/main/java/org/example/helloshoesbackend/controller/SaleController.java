f8905d23f83826915f0fb08d59b757ddbee5a181
Parami <paramiashinsana@gmail.com>
2024-06-12 13:00:44 +0530

    Update

--- src/main/java/org/example/helloshoesbackend/controller/SaleController.java
+++ src/main/java/org/example/helloshoesbackend/controller/SaleController.java
@@ -1,9 +1,10 @@
 package org.example.helloshoesbackend.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.helloshoesbackend.dto.ItemDetailsDTO;
 import org.example.helloshoesbackend.dto.SaleDTO;
-import org.example.helloshoesbackend.dto.SupplierDTO;
 import org.example.helloshoesbackend.service.SaleService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -17,18 +18,39 @@ public class SaleController {
     private final SaleService saleService;
 
     @PostMapping(value = "/saveSale")
+    @PreAuthorize("hasAuthority('USER') or hasAuthority('ADMIN')")
     public SaleDTO saveSales(@RequestBody SaleDTO saleDTO) {
         return saleService.saveSale(saleDTO);
     }
 
     @GetMapping(value = "/getAllSales")
+    @PreAuthorize("hasAuthority('USER') or hasAuthority('ADMIN')")
     List<SaleDTO> getAllSale(){
         return saleService.getAllSales();
     }
 
     @DeleteMapping("/deleteSales/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public void deleteSale(@PathVariable ("id") String id){
         saleService.deleteSales(id);
+    }
+
+    // Only Admin Control
+
+//    @GetMapping("/getTotalSale/{date}")
+//    public Double getTotalSaleItemm(@PathVariable String date){
+//        return saleService.getTotalSaleItem(date);
+//    }
+//
+//    @GetMapping("/getTotalProfit/{date}")
+//    public Double getTotalProfit(@PathVariable String date){
+//        return saleService.getTotalProfit(date);
+//    }
+
+    @GetMapping("/getBestSellingInventory/{date}")
+    @PreAuthorize("hasAuthority('ADMIN')")
+    public ItemDetailsDTO getBestSellingInventory(@PathVariable String date) {
+        return saleService.getBestSellingInventory(date);
     }
 
 }
