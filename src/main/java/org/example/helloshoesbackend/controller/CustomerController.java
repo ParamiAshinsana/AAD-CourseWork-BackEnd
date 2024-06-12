8eafaef641f76d534987d0c540bb17599607102b
Parami <paramiashinsana@gmail.com>
2024-06-12 12:51:45 +0530

    Update

--- src/main/java/org/example/helloshoesbackend/controller/CustomerController.java
+++ src/main/java/org/example/helloshoesbackend/controller/CustomerController.java
@@ -6,6 +6,7 @@ import org.example.helloshoesbackend.service.CustomerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,17 +19,20 @@ public class CustomerController {
     private final CustomerService customerService;
 
     @PostMapping(value = "/saveCustomer")
+    @PreAuthorize("hasAuthority('USER') or hasAuthority('ADMIN')")
     public CustomerDTO saveCustomer(@RequestBody CustomerDTO customerDTO){
         return customerService.saveCustomer(customerDTO);
     }
 
     @PutMapping(value = "/updateCustomer/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public void updateCustomer(@RequestBody CustomerDTO customerDTO, @PathVariable ("id") String id){
         customerService.updateCustomer(id,customerDTO);
         System.out.println("Customer Updated!");
     }
 
     @DeleteMapping("/deleteCustomer/{id}")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public void deleteCustomer(@PathVariable ("id") String id){
         customerService.deleteCustomer(id);
     }
@@ -42,11 +46,13 @@ public class CustomerController {
 //    }
 
     @GetMapping(value = "/getAllCustomer")
+    @PreAuthorize("hasAuthority('USER') or hasAuthority('ADMIN')")
     List<CustomerDTO> getAllCustomers(){
         return customerService.getAllCustomer();
     }
 
     @GetMapping("/getSelectedCustomer/{id}")
+    @PreAuthorize("hasAuthority('USER') or hasAuthority('ADMIN')")
     ResponseEntity<CustomerDTO> getSelectedCustomer(@PathVariable ("id") String id){
         CustomerDTO selectedCustomer = customerService.getSelectedCustomer(id);
         return selectedCustomer != null ? ResponseEntity.ok(selectedCustomer) : ResponseEntity.status(HttpStatus.NOT_FOUND).build();
@@ -55,11 +61,13 @@ public class CustomerController {
 
     // To Sale Service Controller
     @GetMapping(value = "/getCustomerName/{id}")
+    @PreAuthorize("hasAuthority('USER') or hasAuthority('ADMIN')")
     public String getCustomerName(@PathVariable("id")String id ){
         return customerService.getCustomerName(id);
     }
 
     @GetMapping(value = "/getAllCustomerCode")
+    @PreAuthorize("hasAuthority('USER') or hasAuthority('ADMIN')")
     List<String> getAllCustomersCode(){
         return customerService.getAllCustomerCode();
     }
